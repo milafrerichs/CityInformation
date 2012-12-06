de859c052437ace204884d3b44287eb4f0533092
Mila Frerichs <mila.frerichs@gmail.com>
2012-12-06 17:14:51 +0100

    Testing the cloudmade API connection

--- cityInformation.rb
+++ cityInformation.rb
@@ -1,4 +1,7 @@
 require 'rspec'
+require 'net/http'
+require 'json'
+
 class CityInformation
 	attr_accessor :city, :latitude, :longitude
 	def initialize(cityname)
@@ -19,10 +22,29 @@ class Cloudmade
 		@api_key = api_key
 		@api_url = "http://geocoding.cloudmade.com/#{@api_key}/geocoding/v2/find.js"
 	end
-	
-	def find(query)
-		params = {"query"=> query}
+	def connect
+		uri = URI(@api_url)
+		response = Net::HTTP.get_response(uri)
 		
+		case response
+		when Net::HTTPSuccess
+		  response.body
+		else
+		  response.error!
+		end
+		
+	end
+	def parseResponse(response)
+		repo_info = JSON.parse response
+	end
+	def findCity(cityname)
+		params = {"query"=>cityname}
+		buildQuery(params).connect
+	end
+	def buildQuery(params)
+		url_params = URI.escape(params.collect{|k,v| "#{k}=#{v}"}.join('&'))
+		@api_url = "#{@api_url}?#{url_params}"
+		self
 	end
 end
 
--- spec/cloudmade_spec.rb
+++ spec/cloudmade_spec.rb
@@ -5,10 +5,38 @@ describe Cloudmade do
 	its(:api_key) { should_not be_nil }
 	its(:api_url) { should_not be_nil}
 	
-	it 'builds the API query'
-	it 'connects to the service'
-	it 'returns a 200 header'
-	it 'gets a json string'
-	it 'parses the json'
+	it 'builds the API query to match the city name' do
+		params = {"query"=>"Berlin"}
+		expect { subject.buildQuery(params)}.to change{subject.api_url}
+		
+	end
+	it 'can connect to the service' do
+		subject.should respond_to(:connect)
+	end
+	
+	context "conecting" do
+		let(:cloudmade) { Cloudmade.new("okokko") }
+		before {
+			params = {"query"=>"Berlin"}
+			subject.buildQuery(params)
+		}
+		it 'raises an error if api key is wrong empty' do
+			expect { cloudmade.connect}.to raise_error(Net::HTTPServerException)
+		end
+		it 'gets a non empty string' do
+			subject.connect.should_not == ""
+		end
+		
+	end
+	context 'parses the json' do
+		it 'returns valid json' do
+			response = subject.connect
+			expect { subject.parseResponse(response)}.to_not raise_error(JSON::ParserError)
+		end
+		it 'raises an error if response is no json' do
+			response = "hj"
+			expect { subject.parseResponse(response)}.to raise_error(JSON::ParserError)
+		end
+	end
 	it 'returns the lat, long'
 end
